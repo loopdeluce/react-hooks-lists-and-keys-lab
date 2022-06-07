240c8a2b38863ea4d1df44a893414dea35da27a2
Lucy Bullen <102437824+loopdeluce@users.noreply.github.com>
2022-06-07 10:46:15 -0600

    commit tree 38a1c1e1ac47bb95bf1b03fecb4810b59daceb1c

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -2,8 +2,12 @@ import React from "react";
 
 function NavBar() {
   const links = ["home", "about", "projects"];
+  const linksJSX = links.map(link => <a href={'#'+ link} key={link}>{link}</a>)
 
-  return <nav>{/* display an <a> tag for each link here */}</nav>;
+  return (
+  <nav>
+    {linksJSX}
+  </nav>);
 }
 
 export default NavBar;
